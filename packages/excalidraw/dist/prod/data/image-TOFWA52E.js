a579204c1f0868cf85609ec2230eb08d9799eef0
mine.ns <mine.ns@motionx.studio>
2025-12-20 19:37:30 +0700

    chore: Ignore dist folders (build from source)
    Dist folders are now gitignored since they're build artifacts.
    Users should rebuild packages locally with yarn build.

--- packages/excalidraw/dist/prod/data/image-TOFWA52E.js
+++
@@ -1,1 +0,0 @@
-import{T as a,U as b,V as c}from"../chunk-OXT4O7KL.js";import"../chunk-A66AFZZU.js";import"../chunk-SRAX5OIU.js";export{c as decodePngMetadata,b as encodePngMetadata,a as getTEXtChunk};
